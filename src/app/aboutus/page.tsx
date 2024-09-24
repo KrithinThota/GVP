6d3ff60a37bc922ad9bf2f184a39fd67045d1099
KrithinThota <krithinthota11@gmail.com>
2024-09-24 20:00:52 +0530

    build fix

---
+++ src/app/aboutus/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex bg-background items-center justify-center h-screen ">
+        <div className="text-center align-middle ">
+          <p className="dark:text-neutral-200 text-neutral-800 text-3xl">
+            UNDER DEVELOPMENT{" "}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
