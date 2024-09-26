1815f2b63a5c988906f1c622128759de5462d5fa
KrithinThota <krithinthota11@gmail.com>
2024-09-26 09:51:50 +0530

    a few minor changes

---
+++ src/components/ui/datepicker.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface DatePickerProps {
+  selected: Date | null;
+  onChange: (date: Date | null) => void;
+  placeholderText?: string;
+}
+
+const DatePicker = ({
+  selected,
+  onChange,
+  placeholderText,
+}: DatePickerProps) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const date = event.target.value ? new Date(event.target.value) : null;
+    onChange(date);
+  };
+
+  return (
+    <input
+      type="date"
+      value={selected ? selected.toISOString().split("T")[0] : ""}
+      onChange={handleChange}
+      placeholder={placeholderText}
+      className="border rounded p-2"
+    />
+  );
+};
+
+export default DatePicker;
